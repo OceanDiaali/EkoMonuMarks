251eabcca94d96c632b32006b3f288b9bfd6f603
Apple <apple@Apples-MacBook-Pro.local>
2018-02-13 15:56:30 +0100

    add 5 more locations to 'all' array

--- app/monumark.rb
+++ app/monumark.rb
@@ -28,7 +28,12 @@ class MonuMark
             MonuMark.new('National Arts Theatre', 6.4762, 3.3694, "https://en.wikipedia.org/wiki/National_Arts_Theatre"),
             MonuMark.new('Third Mainland Bridge', 6.5025, 3.4025, "https://en.wikipedia.org/wiki/Third_Mainland_Bridge"),
             MonuMark.new('Freedom Park', 6.4489, 3.3965, "https://en.wikipedia.org/wiki/Freedom_Park_(Lagos)"),
-            MonuMark.new('Lekki Conservation Centre', 6.4416, 3.5358, "https://en.wikipedia.org/wiki/Lekki_Conservation_Centre")
+            MonuMark.new('Lekki Conservation Centre', 6.4416, 3.5358, "https://en.wikipedia.org/wiki/Lekki_Conservation_Centre"),
+            MonuMark.new('Tinubu Square', 6.4537, 3.3900, "https://en.wikipedia.org/wiki/Tinubu_Square"),
+            MonuMark.new('National Stadium', 6.4972, 3.3647, "https://en.wikipedia.org/wiki/National_Stadium,_Lagos"),
+            MonuMark.new('Lagos Lagoon', 6.4311, 3.4019, "https://en.wikipedia.org/wiki/Lagos_Lagoon"),
+            MonuMark.new('Taiwo Olowo Monument', 6.4526, 3.3903, "https://en.wikipedia.org/wiki/Taiwo_Olowo"),
+            MonuMark.new('Muritala Mohammed Airport', 6.5818, 3.3211, "https://en.wikipedia.org/wiki/Murtala_Muhammed_International_Airport")
             ]
         end
     end
